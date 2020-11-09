496df9c27e96afbca6841712609cfbfee40bf87a
Nana Lau <nanalau315@hotmail.com>
2020-11-09 05:13:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,26 @@
 # Write your code here!
 def game_hash
+  {:home => {
+    :team_name => "team_name",
+    :colors => ["team_color"],
+    :players => [ 
+      {:player_name => "name",
+      :number => "number",
+      :shoe => "shoe_num",
+      :points => "point",
+      :rebounds => "rebounds",
+      :assists => "assist",
+      :steals => "steals",
+      :blocks => "blocks",
+      :slam_dunks => "dunks"
+        
+        
+        
+        
+      }]
+  },
+    
   
+  
+  :away => {}
 end
